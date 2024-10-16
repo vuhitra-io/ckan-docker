e25c1f13d181b70fc4483e10901b93584a05e2e9
toavina <toavina@vuhitra.io>
2024-10-16 15:17:39 +0000

    tmp:will fix

--- ckan/docker-entrypoint.d/04_sync_ext.sh
+++ ckan/docker-entrypoint.d/04_sync_ext.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-if [ "$STAGE" = "development" ]; then
-  /root/watch_sync.sh &
-  echo "We are in development stage, syncing ..."
-else
-    echo "We are not in development stage, no syncing ..."
-fi
+/root/watch_sync.sh &
+
